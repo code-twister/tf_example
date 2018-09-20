5b1509ac308ffa72609f1a3fd7afdcd0123b4f9a
codetwister <mihaly.f.nagy@gmail.com>
2018-09-21 00:50:51 +0300

    removing some code to make for a good starting point

--- src/main/java/ro/softvision/tensorflow/demo/detection/classifier/impl/TensorFlowObjectDetectionAPIModel.java
+++ src/main/java/ro/softvision/tensorflow/demo/detection/classifier/impl/TensorFlowObjectDetectionAPIModel.java
@@ -7,6 +7,7 @@ import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import ro.softvision.tensorflow.demo.detection.classifier.Classifier;
@@ -24,56 +25,14 @@ import static ro.softvision.tensorflow.demo.detection.classifier.impl.TensorFlow
 
 public class TensorFlowObjectDetectionAPIModel implements Classifier {
 
-    private static final int MAX_RESULTS = 100;
-
-    private TensorFlowInferenceInterface tensorFlowInferenceInterface;
-
-    private ArrayList<String> labels = new ArrayList<>();
-
-    private int inputSize;
-    private float[] detectionBoxes;
-    private float[] detectionClasses;
-    private float[] detectionScores;
-    private float[] numDetection;
-
-    private int[] pixelBuffer;
-    private byte[] bufferByChannel;
-
-    private ArrayList<Recognition> recognitions = new ArrayList<>();
 
     public TensorFlowObjectDetectionAPIModel(AssetManager assetManager, String modelFileName, String labelsFileName, int inputSize) throws IOException {
-        this.inputSize = inputSize;
 
-        initializeLabels(labels, assetManager, labelsFileName);
-
-        tensorFlowInferenceInterface = new TensorFlowInferenceInterface(assetManager, modelFileName);
-
-        pixelBuffer = new int[inputSize * inputSize];
-        bufferByChannel = new byte[inputSize * inputSize * 3];
-
-        detectionBoxes = new float[MAX_RESULTS * 4];
-        detectionClasses = new float[MAX_RESULTS];
-        detectionScores = new float[MAX_RESULTS];
-        numDetection = new float[1];
     }
 
     @Override
     public List<Recognition> recognizeImage(Bitmap bitmap) {
-
-        processBitmap(bitmap, pixelBuffer, bufferByChannel);
-
-        tensorFlowInferenceInterface.feed(INPUT_NAME, bufferByChannel, 1, inputSize, inputSize, 3);
-
-        tensorFlowInferenceInterface.run(OUTPUTS);
-
-        tensorFlowInferenceInterface.fetch(OUTPUT_DETECTION_BOXES, detectionBoxes);
-        tensorFlowInferenceInterface.fetch(OUTPUT_DETECTION_CLASSES, detectionClasses);
-        tensorFlowInferenceInterface.fetch(OUTPUT_DETECTION_SCORES, detectionScores);
-        tensorFlowInferenceInterface.fetch(OUTPUT_NUM_DETECTION, numDetection);
-
-        populateRecognitions(recognitions, Math.round(numDetection[0]), labels, detectionClasses, detectionScores, detectionBoxes, inputSize);
-
-        return recognitions;
+        return Collections.emptyList();
     }
 
 }
